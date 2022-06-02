9efec88372d47bd116e4d79bfe005731b8b3cbf0
pbringetto <plbriqto@gmail.com>
2022-06-01 22:03:09 -0700

    first commit

---
+++ src/ta_signals/__init__.py
@@ -0,0 +1,16 @@
+from .main import TaSignals
+
+def go(ohlc, key):
+    t = TaSignals()
+
+    ma_data, ohlc = t.ma(ohlc, key)
+    ema_data, ohlc = t.ema(ohlc, key)
+    bollinger_data, ohlc = t.bollinger(ohlc, key)
+    rsi_data, ohlc = t.rsi(ohlc, key)
+    macd_data, ohlc = t.macd_slope(ohlc, key)
+    div_data, ohlc = t.divergence(ohlc, key)
+    obv_data, ohlc = t.on_balance_volume(ohlc, 5, key)
+    data = div_data + bollinger_data + macd_data + ma_data + ema_data
+
+    return data
+
